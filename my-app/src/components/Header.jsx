5110805c4270d477c17b459d01ac3ac8920c06bc
Timofey-Byunenko1 <byunenkotimofey@gmail.com>
2026-04-17 09:31:59 +0300

    fix react

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import { AppBar, Toolbar, Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
--- my-app/src/pages/Todo.jsx
+++ my-app/src/pages/Todo.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import { useState } from "react";
 import { Container, TextField, Button, List, ListItem } from "@mui/material";
 
